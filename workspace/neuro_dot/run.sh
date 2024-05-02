192173212694ef7ce3fd7fe718885dc20e9ec7cd
ythackerCS <46691555+ythackerCS@users.noreply.github.com>
2024-05-02 12:31:19 -0500

    Updated container to output as assessor

--- workspace/neuro_dot/run.sh
+++ workspace/neuro_dot/run.sh
@@ -11,16 +11,28 @@ echo $@
 SUBJECT_MAT=$1
 shift
 PARAMS=$1
+shift
+SCAN_ID=$1
+shift
+SESSION_ID=$1
+shift
+SESSION_LABEL=$1
+shift
+PROJECT=$1
+shift
+XNAT_USER=$1
 
 dt=`date --utc +%Y%m%d-%H%M%S`
 
 echo dt 
 
-OUTPUTFOLDER_NOTEBOOK="/outputfiles/${dt}_preprocessing_notebook"
-OUTPUTFOLDER_IMAGES="/outputfiles/${dt}_images/"
+OUTPUTFOLDER_NOTEBOOK="/outputfiles/preprocessing_notebook"
+OUTPUTFOLDER_IMAGES="/outputfiles/images/"
 
 mkdir $OUTPUTFOLDER_NOTEBOOK $OUTPUTFOLDER_IMAGES
 
 echo 'papermill neuro_dot/NeuroDOT_PreProcessing_Notebook.ipynb $OUTPUTFOLDER_NOTEBOOK/output.ipynb -p patient_data /input/$SUBJECT_MAT -p params_file /input/$PARAMS -p saveImagePath $OUTPUTFOLDER_IMAGES'
 
 papermill neuro_dot/NeuroDOT_PreProcessing_Notebook.ipynb $OUTPUTFOLDER_NOTEBOOK/output.ipynb -p participant_data /input/$SUBJECT_MAT -p params_file /input/$PARAMS -p saveImagePath $OUTPUTFOLDER_IMAGES
+
+python neuro_dot/makeXML.py --scanID $SCAN_ID --sessionID $SESSION_ID  --sessionLabel $SESSION_LABEL --project $PROJECT --xnat_user $XNAT_USER
